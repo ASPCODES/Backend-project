7e6be25496f2c878520f8bafc7acd54435da553b
ASPCODES <190011700008adityasinghmechgpa@gmail.com>
2024-04-18 00:20:19 +0530

    add controllers and debug the code

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,14 +34,13 @@ const userSchema  = new Schema(
         },
         watchHistory: [
             {
-                type: Schema.Types.objectId,
+                type: Schema.Types.ObjectId,
                 ref: "Video"
             }
         ],
         password: {
             type: String,
-            required: [true, "Password is mandatory"],
-            unique: true
+            required: [true, "Password is mandatory"]
         },
         refreshToken: {
             type: String
@@ -55,7 +54,7 @@ const userSchema  = new Schema(
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 12)
+    this.password = await bcrypt.hash(this.password, 12)
     next()
 })
 
